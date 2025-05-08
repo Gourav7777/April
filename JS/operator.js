4b29cd3c73e35aa64d75f57093216de1f9e2fcc4
Gourav7777 <360inside360@gmail.com>
2025-05-08 22:05:03 +0530

    operators-loops

---
+++ JS/operator.js
@@ -0,0 +1,145 @@
+
+
+
+
+let marks =prompt("Enter marks")
+
+marks = Number(marks)
+
+switch(marks){
+
+    case 40:
+        console.log("B")
+     break;
+
+     case 50:
+        console.log("A")
+        break;
+
+        default :
+        console.log("C")
+
+}
+
+
+
+// do{
+//     console.log(j)
+//     // j+=3
+// }while(j)
+
+
+// a>b?console.log("A"):  console.log("B")
+
+//  let age;
+//  let marriageAge = 21
+
+
+//  let loggedin = false
+
+
+//    if(!loggedin){
+//     console.log("please log in")
+//    }
+//    else{
+//     console.log("welcome");
+    
+//    }
+
+//  let grade
+//  let marks = 23
+//   if(marks && grade){
+//    // any one condition can be true
+//     console.log("true or correct")
+//    }
+
+
+//    else{
+//     console.log("incorrect")
+//    }
+
+
+//   age = prompt("Enter your age")
+
+
+//    if(age>=21){
+//     console.log("Marriage Allowed")
+//    }
+
+//    else{
+//     console.log("Not allowed")
+//    }
+
+
+
+
+
+
+
+
+ // >= 21
+//   if(a>b){
+//     console.log("a")
+//   }
+//   else if(a==b){
+//   console.log("equals")
+//   }
+
+//   else{
+//     console.log("b")
+//   }
+
+
+
+
+
+
+
+
+
+
+// console.log(a!=b) 
+
+// console.log(a!==b) // datatype + values
+
+
+// console.log(a===b) // false
+// console.log(a==b) // false
+// console.log(a>b) // true
+// console.log(a>=b) // true
+// console.log(a<b) // false
+// console.log(a<=b) // false
+
+
+// console.log(a!=b) // true
+// console.log(a!==b) // false
+
+
+
+
+
+
+
+
+
+
+
+// console.log(9+7) // 16
+
+// console.log(9-7) // 2
+
+// console.log(9*7) // 63
+
+// console.log(9/7) // 1....
+
+// console.log(9%7) // 2
+
+// console.log(a++) // 10
+
+// console.log(b--) //5
+
+// console.log(a) //  // 11
+
+// console.log(b) // 4
+
+// console.log(++a) //12
