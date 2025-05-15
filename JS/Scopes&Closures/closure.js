5e1039992cd55839d3efec3d3cba13613332f3c9
Gourav7777 <360inside360@gmail.com>
2025-05-16 05:16:37 +0530

    scope

---
+++ JS/Scopes&Closures/closure.js
@@ -0,0 +1,28 @@
+
+var a = 1;
+function foo() {
+  console.log(a);
+  var a = 2;
+}
+foo();
+
+
+
+
+// function outer(){
+//     let a = 10
+
+//       let b =20
+//    return function inner(){
+
+       
+//      console.log( a)
+//     }
+
+    
+// }
+    
+
+// let returnedValue = outer()
+
+// returnedValue()
---
+++ JS/Scopes&Closures/scope.js
@@ -0,0 +1,28 @@
+
+let a =true
+
+{
+    let blockVariable = 10
+    console.log(blockVariable)
+}
+
+console.log(a)
+
+
+
+// function display(){
+
+//     // Local Scope
+//    var b = true // undefined
+//     let a = 20 // unavaial...
+
+//     // Block scope
+//     {
+
+//      let a = 10 // unavailable
+//      console.log(a,"inside block")
+//     }
+//  console.log(b,a , "outside block")
+// }
+
+// display()
