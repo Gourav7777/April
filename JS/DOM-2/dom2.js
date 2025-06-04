f88d4de25821e3ccf87ba1759aa3b5b81b9acc78
Gourav7777 <360inside360@gmail.com>
2025-06-04 21:08:06 +0530

    dom-2

---
+++ JS/DOM-2/dom2.js
@@ -0,0 +1,92 @@
+
+
+// let h2 = document.querySelector("h2")
+
+// h2.classList.add("x","y","z")
+
+
+// let returned = h2.classList.toggle("x")
+
+// console.log(returned);
+
+// let selected = document.querySelector("#selected")
+
+// let lii = selected.parentElement.parentElement.nextElementSibling.children[1]
+
+// lii.style.color="red"
+// console.log(selected.parentElement.parentElement.nextElementSibling.children[1]);
+
+
+// let h1 = document.createElement("h1")
+// h1.innerText ="i am new heading-1"
+
+// let h2 = document.createElement("h2")
+// h2.innerText ="i am heading-2"
+
+
+//   let section = document.getElementById("sec")
+
+// //   section.appendChild(h1,h2)
+
+
+//   section.append(h1,h2)
+
+//   section.remove()
+// //   section.removeChild(h1)
+
+// // section.innerHTML =  "<h1>i am</h1>"
+   
+// console.log(h1);
+
+
+
+// let btn = document.getElementById("btn")
+
+// function clicked(){
+//     console.log("i am clicked");
+    
+// }
+
+// function Doubleclicked(){
+//     console.log("i am double clicked");
+    
+// }
+// // btn.onclick = clicked
+// // btn.onclick = Doubleclicked
+
+
+// btn.addEventListener("click",function(){
+//     clicked()
+//     Doubleclicked()
+// })
+
+
+// document.getElementById("r").addEventListener("click",()=>{
+
+//     document.querySelector("body").style.backgroundColor="red"
+// })
+
+// document.getElementById("g").addEventListener("click",()=>{
+
+//     document.querySelector("body").style.backgroundColor="green"
+// })
+
+// document.getElementById("b").addEventListener("click",()=>{
+
+//     document.querySelector("body").style.backgroundColor="blue"
+// })
+
+// let h1 = document.querySelector("h1")
+
+// document.querySelector("#i").addEventListener("input",(event)=>{
+    
+    
+//     // h1.innerText =  event.target.value
+//     console.log(event.which,event);
+// })
+
+// document.querySelector("#i").addEventListener("keydown", (e) => {
+//     if (e.key == 'Enter'){
+//         console.log(e.target.value)
+//     }
+// })
